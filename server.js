2dae9b7ce63c5174b43fb32fb7ea86bd34e9527b
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-02-07 18:51:49 +0100

    feature: add cors and .env support

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+const cors = require("cors");
+// console.log(process.env);
+const port = process.env.PORT;
 
-// serve static files.  It’s safer to use the absolute path of the directory that you want to serve.
-// path.join(__dirname, 'public'): This part of the code constructs the absolute path to the 'public' directory. __dirname is a global variable in Node.js that represents the current directory of the module (the directory where the current script resides). path.join is used to concatenate this directory with the 'public' folder to create the absolute path to the static files.
-
+app.use(cors({ origin: `http://localhost:${port}` }));
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    return res.send(`Welcome to my Express.js server`);
+    return res.send(`This is main page`);
 });
 
-app.get("/about", (req, res) => {
-    return res.send(
-        `Not much to say. I miss the sun and need to get hired. Soon.`
-    );
+app.get("/welcome", (req, res) => {
+    return res.send(`Welcome.`);
 });
 
-// go example route http://localhost:3000/greet?name=John&lastName=Smith
 app.get("/greet", (req, res) => {
     const { name, lastName } = req.query;
 
